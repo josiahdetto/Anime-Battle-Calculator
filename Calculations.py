5aad5e57d77880f369685b2ebdaaf33041722688
josiahdetto <60636495+josiahdetto@users.noreply.github.com>
2020-07-17 12:36:03 -0400

    Update Calculations.py

--- Calculations.py
+++ Calculations.py
@@ -1,71 +1,70 @@
 class Calculations:
+    
+    max_total = 8.3
+    
     def __init__(self, state, data):
         self.data = data
         self.state = state
-        """if state:
-            pre_battle()
-        else:
-            during_battle()"""
 
     def character_role_score(self):
         role = self.data[0]
-        if role == 1:
-            return 10
-        else if role == 2:
-            return 5
-        else:
-            return 1
+        return role
     def character_trained_score(self):
         trained = self.data[1]
-        if trained == 1:
-            return 10
-        else:
-            return 0
+        return trained
     def enemy_role_score(self):
         role = self.data[2]
-        if role == 1:
-            return 10
-        else if role == 2:
-            return 5
-        else:
-            return 1
-    def calc_lost_score(times_lost):
+        return role
+    def calc_lost_score(self):
+        times_lost = self.data[4]
         if times_lost <= 3:
             return 2 ** times_lost
         else:
-            return 4 
+            return 4
+    def calc_timeskip_score(self):
+        battles_fought = self.data[6]
+        if battles_fought == 0:
+            return 5
+        else:
+            return 1
     
     def character_lost_score(self):
         lost = self.data[3]
-        if type(lost) is bool:
+        if lost == 0:
             return 0
         else:
-            return calc_lost_score(lost)
+            return self.calc_lost_score()
+
+    def post_timeskip_score(self):
+        timeskip = self.data[5]
+        if timeskip == 0:
+            return 0
+        else:
+            return self.calc_timeskip_score()
 
     def protect_friends_score(self):
-        protect = self.data[4]
-        if protect:
-            return 10
-        else:
-            return 0
+        protect = self.data[7]
+        return protect
 
     def calculate_pre_battle(self):
-        char_role_weight = 0.3
-        trained_weight = 0.2
-        enemy_role_weight = 0.5
-        lost_weight = 0.25
-        protect_weight = 0.35
+        char_role_weight = 0.5
+        trained_weight = 0.3
+        enemy_role_weight = 0.7 
+        lost_weight = 0.2
+        timeskip_weight = 0.5
+        protect_weight = 0.3
 
         char_role_score = self.character_role_score()
         trained_score = self.character_trained_score()
         enemy_role_score = self.enemy_role_score()
         lost_score = self.character_lost_score()
+        timeskip_score = self.post_timeskip_score()
         protect_score = self.protect_friends_score()
         
-        return ((char_role_weight * char_role_score) + (trained_weight *
+        total = ((char_role_weight * char_role_score) + (trained_weight *
                 trained_score) + (enemy_role_weight * enemy_role_score) +
-                (lost_weight * lost_score) + (protect_weight * protect_score)) /
-                1.6
+                (lost_weight * lost_score) + (protect_weight * protect_score))
+        return total/Calculations.max_total
                 
     def calculate_during_battle(self):
         pass
@@ -76,10 +75,11 @@ class Calculations:
         else:
             return self.calculate_pre_battle()
 
-    def get_verdict():
+    def get_verdict(self):
         score = self.get_score()
+        print(score)
 
-        if score > 0.5:
+        if round(score, 2) >= 0.5:
             return True
         else:
             return False
